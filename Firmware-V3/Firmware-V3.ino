7f4632c3566f9c46b18c2f6601e66e401d630967
Ghost Note Audio <94485822+GhostNoteAudio@users.noreply.github.com>
2024-06-02 13:19:05 +0100

    more work on new firmware. Default settings, magic constant to detect if programmed and more

--- Firmware-V3/Firmware-V3.ino
+++ Firmware-V3/Firmware-V3.ino
@@ -5,9 +5,9 @@
 
 // Change if using log slider
 #define LINEAR_SLIDER 1
-#define MAX_SYX_LEN 150
-#define EXPECTED_SYX_LEN 95 // 13 + 1 + 9 * 9
+#define MAX_SYX_LEN 500
 #define BYTES_PER_MAPPING 9
+#define MAGIC_CONSTANT 3788608666 // Used to check whether the unit has been programmed or not
 
 // USB MIDI object
 Adafruit_USBD_MIDI usb_midi;
@@ -23,6 +23,9 @@ public:
     uint8_t Channel = 0;
     uint8_t SubmitThreshold = 5;
 
+    uint8_t FaderBottomDeadZone = 5;
+    uint8_t FaderTopDeadZone = 5;
+
     uint16_t ParamNumber = 1; // CC or nrpn number
     uint16_t MinVal = 0;
     uint16_t MaxVal = 127;
@@ -33,6 +36,7 @@ public:
 
     void ProcessUpdate(float filteredValue, bool dryrun)
     {
+        //Serial.printf("Process update - Mapping. Value: %.2f, dryrun: %d\n", filteredValue, dryrun ? 1 : 0);
         if (SendCC)
             SendCCUpdate(filteredValue, dryrun);
     }
@@ -42,14 +46,14 @@ public:
         buffer[0] = Channel;
         buffer[1] = SubmitThreshold;
         
-        buffer[2] = (ParamNumber >> 7) & 0x7F;
+        buffer[2] = (uint8_t)((ParamNumber >> 7) & 0x7F);
         buffer[3] = (ParamNumber) & 0x7F;
 
-        buffer[4] = (MinVal >> 7) & 0x7F;
-        buffer[5] = (MinVal) & 0x7F;
+        buffer[4] = (uint8_t)((MinVal >> 7) & 0x7F);
+        buffer[5] = (uint8_t)((MinVal) & 0x7F);
 
-        buffer[6] = (MaxVal >> 7) & 0x7F;
-        buffer[7] = (MaxVal) & 0x7F;
+        buffer[6] = (uint8_t)((MaxVal >> 7) & 0x7F);
+        buffer[7] = (uint8_t)((MaxVal) & 0x7F);
         
         buffer[8] = SendPitchbend * 4;
         buffer[8] |= SendNrpn * 2;
@@ -83,14 +87,18 @@ private:
 
     void SendCCUpdate(float filteredValue, bool dryrun)
     {
+        int dr = dryrun ? 1 : 0;
+
         if (fabsf(filteredValue - lastTransmitFloat) >= SubmitThreshold)
         {
-            int midiValue = ((int)roundf(filteredValue)) >> 3;
+            //Serial.printf("Sending CC update - filteredValue: %.2f, lastTransmitFloat: %.2f, SubmitThreshold: %d, dryrun: %d \n", filteredValue, lastTransmitFloat, SubmitThreshold, dr);
+
+            int midiValue = ((int)(filteredValue)) >> 0;
             if (lastTransmitCC != midiValue)
             {
                 lastTransmitFloat = filteredValue;
                 lastTransmitCC = midiValue;
-                Serial.printf("Submitting %d (%.2f)\n", midiValue, lastTransmitFloat);
+                Serial.printf("Submitting midiValue %d, lastTransmitCC: %d\n", midiValue, lastTransmitCC);
                 if (!dryrun)
                     WriteCc(midiValue);
             }
@@ -103,30 +111,37 @@ private:
         data[0] = Channel | 0xB0;
         data[1] = ParamNumber;
         data[2] = midiValue;
+        //Serial.println("Writing 3 bytes to usb midi port");
         usb_midi.write(data, 3);
+        //Serial.println("Done writing");
     }
 };
 
 class FaderProcessor
 {
+    Mapping* mappings;
+
 public:
-    int FaderMinVal = 5;
-    int FaderMaxVal = 1020;
+    FaderProcessor(Mapping* mappings)
+    {
+        this->mappings = mappings;
+    }
 
     int adcValues[3];
     float floatValues[3];
     float filteredValues[3];
 
-    void ProcessUpdate(int adcValue, int idx)
+    void ProcessUpdate(int adcValue, int idx, int page)
     {
+        auto mapping = mappings[page * 3 + idx];
+        //Serial.printf("ProcessUpdate - Fader %d, value %d\n", idx, adcValue);
         adcValues[idx] = adcValue;
-        adcValue = adcValue - FaderMinVal;
-        float range = FaderMaxVal - FaderMinVal;
+        adcValue = adcValue - mapping.FaderBottomDeadZone;
+        float range = (1023 - mapping.FaderTopDeadZone) - mapping.FaderBottomDeadZone;
         float fVal = adcValue / range;
         if (fVal < 0) fVal = 0.0f;
-        if (fVal > 1) fVal = 1.0f;
+        if (fVal > 1) fVal = 1.0f;;
         floatValues[idx] = fVal * 1023.0f;
-
         FilterValue(idx);
     }
 
@@ -142,15 +157,14 @@ private:
     {
         float alpha;
         float val = floatValues[i];
-
         // larger alpha if new value is far away, so we seek the correct location quicker
         // Slower alpha when nearby to smooth noise
         if (fabsf(val - filteredValues[i]) > 10)
           alpha = 0.05;
         else
           alpha = 0.005;
-
         filteredValues[i] = (1-alpha) * filteredValues[i] + alpha * val;
+        Serial.printf("Filtered fader %d: %.2f\n", i, filteredValues[i]);
     }
 };
 
@@ -189,6 +203,9 @@ public:
             btnState = 0;
         }
 
+        if (pageChanged)
+            Serial.printf("New Page: %d\n", page);
+
         SetPageLed();
         return pageChanged;
     }
@@ -197,8 +214,6 @@ private:
     void SetPageLed()
     {
         // Control LED colour
-        Serial.printf("Current Page: %d", page);
-
         if (page == 0)
         {
             digitalWrite(D6, 1);
@@ -235,14 +250,66 @@ public:
     void PrintSettings()
     {
         for (int i=0; i<9; i++)
-            mappings->Print();
+            mappings[i].Print();
+    }
+
+    bool IsProgrammed()
+    {
+        uint8_t b0 = (MAGIC_CONSTANT >> 24) & 0xFF;
+        uint8_t b1 = (MAGIC_CONSTANT >> 16) & 0xFF;
+        uint8_t b2 = (MAGIC_CONSTANT >> 8) & 0xFF;
+        uint8_t b3 = (MAGIC_CONSTANT >> 0) & 0xFF;
+
+        bool output = EEPROM[0] == b0 && EEPROM[1] == b1 && EEPROM[2] == b2 && EEPROM[3] == b3;
+        Serial.printf("Unit is programmed: %d\n", output ? 1 : 0);
+        return output;
+    }
+
+    void SetProgrammed(bool commit = false)
+    {
+        uint8_t b0 = (MAGIC_CONSTANT >> 24) & 0xFF;
+        uint8_t b1 = (MAGIC_CONSTANT >> 16) & 0xFF;
+        uint8_t b2 = (MAGIC_CONSTANT >> 8) & 0xFF;
+        uint8_t b3 = (MAGIC_CONSTANT >> 0) & 0xFF;
+
+        EEPROM[0] = b0;
+        EEPROM[1] = b1;
+        EEPROM[2] = b2;
+        EEPROM[3] = b3;
+        Serial.println("Setting IsProgrammed Flag magic constants");
+        if (commit)
+            EEPROM.commit();
+    }
+
+    void LoadDefaultSettings(bool storeSettings = true)
+    {
+        Serial.println("Loading default settings programmatically...");
+
+        for (int i = 0; i < 9; i++)
+        {
+            mappings[i].Channel = 0;
+            mappings[i].SubmitThreshold = 5;
+            mappings[i].ParamNumber = 1 + i;
+            mappings[i].MinVal = 0;
+            mappings[i].MaxVal = 127;
+            mappings[i].SendPitchbend = false;
+            mappings[i].SendNrpn = false;
+            mappings[i].SendCC = true;
+        }
+
+        if (storeSettings)
+            StoreSettings();
     }
 
     void LoadSettingsFromEEPROM()
     {
+        uint8_t temp[256];
+        for (int i = 0; i < 256; i++)
+            temp[i] = EEPROM[4+i];
+        
         Serial.println("Loading settings from flash memory...");
         for (int i = 0; i < 9; i++)
-            mappings[i].LoadData(&EEPROM[i*BYTES_PER_MAPPING]);
+            mappings[i].LoadData(&temp[i*BYTES_PER_MAPPING]);
         PrintSettings();
     }
 
@@ -264,25 +331,31 @@ public:
 
         PrintSettings();
 
+        SetProgrammed();
+
         for (int i = 0; i < 9; i++)
         {
-            mappings[i].PackData(&EEPROM[i*BYTES_PER_MAPPING]);
+            mappings[i].PackData(&EEPROM[4+i*BYTES_PER_MAPPING]);
         }
 
         // Only calling this manually because this operation can take a while and we don't want to fill the buffer
         TinyUSB_Device_FlushCDC();
         TinyUSB_Device_Task();
 
+        Serial.println("Commit eeprom");
         EEPROM.commit();
 
         delay(200);
+        Serial.println("Reload stored settings from eeprom");
         LoadSettingsFromEEPROM();
     }
 
     void ListenForSysexInput()
     {
+        //Serial.println("Listen for Sysex input...");
         while (usb_midi.available() > 0)
         {
+            Serial.println("Data available");
             uint8_t value = usb_midi.read();
 
             if (value == 0xF0)
@@ -300,6 +373,8 @@ public:
                 AppendSysex(value);
             }
         }
+
+        //Serial.println("Exiting sysex listener function");
     }
 
     void BeginSysex()
@@ -310,8 +385,7 @@ public:
 
     void AppendSysex(uint8_t value)
     {
-        Serial.print("Appending sysex value: ");
-        Serial.println(value);
+        Serial.printf("Appending sysex value: %d\n", value);
         // example message (use as default values for programmed units):
         // F0  7E 67 68 6F 73 74 6E 6F 74 65 00 02  ... F7
         // SYX ID g  h  o  s  t  n  o  t  e  DEV-ID ... SYX
@@ -322,8 +396,16 @@ public:
         sysexCount++;
     }
 
-    bool ValidateSysex()
+    bool ValidateSysex(int dataLen)
     {
+        Serial.println("Validating Sysex message");
+
+        if (dataLen < 14)
+        {
+            Serial.println("Received incomplete Sysex Message.");
+            return false;
+        }
+
         bool valid = true;
         valid &= sysexData[0] == 0xF0; // SYX start
         valid &= sysexData[1] == 0x7E; // dummy Manufacturer Id
@@ -338,7 +420,7 @@ public:
         valid &= sysexData[10] == 0x65; // e
         valid &= sysexData[11] == 0x00; // DEV-ID byte0
         valid &= sysexData[12] == 0x02; // DEV-ID byte1 - changed from previous Conductor version
-        valid &= sysexData[EXPECTED_SYX_LEN-1] == 0xF7; // Sysex End
+        valid &= sysexData[dataLen-1] == 0xF7; // Sysex End
         return valid;
     }
 
@@ -348,11 +430,6 @@ public:
         int dataLen = sysexCount;
         sysexCount = 0;
 
-        if (dataLen != EXPECTED_SYX_LEN)
-        {
-            Serial.println("Received incomplete Sysex Message. Not processing.");
-            return;
-        }
 
         // Print the message
         for (int i = 0; i < 21; i++)
@@ -362,20 +439,21 @@ public:
         }
         Serial.println("");
 
-        bool valid = ValidateSysex();
+        bool valid = ValidateSysex(dataLen);
         if (!valid)
         {
             Serial.println("Invalid Sysex message received");
             return;
         }
 
+        Serial.println("Sysex valid, proceeding to load settings from buffer...");
         LoadSettingsFromSysexData();
     }
 };
 
 ButtonProcessor button;
-FaderProcessor faders;
 Mapping mappings[9];
+FaderProcessor faders(mappings);
 ConfigManager configManager(mappings);
 int iterations;
 
@@ -405,9 +483,15 @@ void setup()
     // wait until device mounted
     while( !TinyUSBDevice.mounted() ) delay(1);
 
+    while(!Serial) {}
+
     delay(100);
     EEPROM.begin(512);
-    configManager.LoadSettingsFromEEPROM();
+
+    if (configManager.IsProgrammed())
+        configManager.LoadSettingsFromEEPROM();
+    else
+        configManager.LoadDefaultSettings();
 }
 
 void loop()
@@ -416,16 +500,15 @@ void loop()
     bool changePage = button.ProcessPageButton(digitalRead(D2));
     if (changePage) iterations = 0;
     bool dryrun = iterations < 100;
-    
-    faders.ProcessUpdate(analogRead(A2), 0);
-    faders.ProcessUpdate(analogRead(A1), 1);
-    faders.ProcessUpdate(analogRead(A0), 2);
-
     int page = button.GetPage();
-    mappings[page * 3 + 0].ProcessUpdate(faders.GetFilteredValue(0), dryrun);
-    mappings[page * 3 + 1].ProcessUpdate(faders.GetFilteredValue(1), dryrun);
-    mappings[page * 3 + 2].ProcessUpdate(faders.GetFilteredValue(2), dryrun);
 
+    faders.ProcessUpdate(analogRead(A2), 0, page);
+    //faders.ProcessUpdate(analogRead(A1), 1, page);
+    //faders.ProcessUpdate(analogRead(A0), 2, page);
+    
+    mappings[page * 3 + 0].ProcessUpdate(faders.GetFilteredValue(0), dryrun);
+    //mappings[page * 3 + 1].ProcessUpdate(faders.GetFilteredValue(1), dryrun);
+    //mappings[page * 3 + 2].ProcessUpdate(faders.GetFilteredValue(2), dryrun);
     configManager.ListenForSysexInput();
     delayMicroseconds(100);
 }
